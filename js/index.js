df33018655d83c3b00a151c6626012d4e09b76d1
shaunvan119 <shaunvan@live.com.au>
2022-03-02 14:12:03 +1100

    started hand function in JS

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+const pickUserHand = (hand) => {
+    console.log(hand);
+    //hide current before moving onto next page
+    let hands = document.querySelector(".hands")
+    hands.style.display = "none";
+    //show the next page with the hand we pick and 
+}
