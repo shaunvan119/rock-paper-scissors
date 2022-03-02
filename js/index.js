afd70259f24793b74b4445051a80e159b0feeb8d
shaunvan119 <shaunvan@live.com.au>
2022-03-02 18:32:56 +1100

    added contest function

--- js/index.js
+++ js/index.js
@@ -6,4 +6,7 @@ const pickUserHand = (hand) => {
     let hands = document.querySelector(".hands")
     hands.style.display = "none";
     //show the next page with the hand we pick and 
+
+    let contest = document.querySelector(".contest");
+    contest.style.display = "flex";
 }
