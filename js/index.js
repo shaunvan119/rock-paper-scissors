0b5d5efb66b089f53c3471ba95ba87f8146a505c
shaunvan119 <shaunvan@live.com.au>
2022-03-03 12:55:24 +1100

    last commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,82 @@
+
+    //dictionary data structure mapping
 const handOptions = {
     "rock" : "images/Rock.png",
     "paper" :"images/Paper.png",
     "scissors" :"images/Scissors.png"
 }
 
+let SCORE = 0;
+
 const pickUserHand = (hand) => {
-    console.log(hand);
-    //hide current before moving onto next page
-    let hands = document.querySelector(".hands")
-    hands.style.display = "none";
-    //show the next page with the hand we pick and 
+  let hands = document.querySelector(".hands");
+  hands.style.display = "none";
 
-    let contest = document.querySelector(".contest");
-    contest.style.display = "flex";
+  let contest = document.querySelector(".contest");
+  contest.style.display = "flex";
 
-    // set the user pick
-    if( hand == "rock") {
-    document.getElementById("userPickImage").src = "images/Rock.png"
- }
+  // set user Image
+  document.getElementById("userPickImage").src = handOptions[hand];
 
+  pickComputerHand(hand);
+};
+
+const pickComputerHand = (hand) => {
+    let hands = ["rock", "paper", "scissors"];
+    let cpHand = hands[Math.floor(Math.random() * hands.length)];
+    
+    // set computer image 
+    document.getElementById("computerPickImage").src = handOptions[cpHand]
+    
+    referee(hand, cpHand);
+};
+
+const referee = (userHand, cpHand) => {
+  if (userHand == "paper" && cpHand == "scissors") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "paper" && cpHand == "rock") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+  }
+  if (userHand == "paper" && cpHand == "paper") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "rock" && cpHand == "scissors") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+  }
+  if (userHand == "rock" && cpHand == "paper") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "rock" && cpHand == "rock") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "scissors" && cpHand == "scissors") {
+    setDecision("It's a tie!");
+  }
+  if (userHand == "scissors" && cpHand == "rock") {
+    setDecision("YOU LOSE!");
+  }
+  if (userHand == "scissors" && cpHand == "paper") {
+    setDecision("YOU WIN!");
+    setScore(SCORE + 1);
+  }
+};
+
+const restartGame = () => {
+  let contest = document.querySelector(".contest");
+  contest.style.display = "none";
+
+  let hands = document.querySelector(".hands");
+  hands.style.display = "flex";
+}
+
+const setDecision = (decision) => {
+  document.querySelector(".decision h1").innerText = decision;
+}
+
+const setScore = (newScore) => {
+  SCORE = newScore;
+  document.querySelector(".score h1").innerText = newScore;
 }
